70dfcf94195853d36b7a79a494f7ac451db5d2d0
emreoztr <ozturk.yemr@gmail.com>
2021-02-28 19:11:30 +0300

    adds route feature (has some bugs)

--- app/src/main/java/com/example/crowdedhackathon/HeatMapCalc.java
+++ app/src/main/java/com/example/crowdedhackathon/HeatMapCalc.java
@@ -2,12 +2,15 @@ package com.example.crowdedhackathon;
 
 import android.app.Application;
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.PolyUtil;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
 import com.google.maps.android.heatmaps.WeightedLatLng;
 
@@ -21,7 +24,8 @@ public class HeatMapCalc{
     private double correctLongitude;
     private final double correctLatitude = 110574;
 
-    HeatMapCalc(GoogleMap map, ArrayList<LatLng> markers, double rad, double collVal) throws Exception {
+    HeatMapCalc(GoogleMap map, ArrayList<LatLng> markers, double rad, double collVal, int mode) throws Exception {
+        final int option = mode;
         if (markers.size() != 2 || collVal > rad || map == null)
             throw (new Exception());
         if (markers.get(0).latitude > markers.get(1).latitude) {
@@ -46,36 +50,102 @@ public class HeatMapCalc{
 
         //calculates length between centers of circles
         double lenbtCircles = 2 * rad - collVal;
-
-        ArrayList<LatLng> circleLoc = new ArrayList<>();
-        for (int i = 0; lenbtCircles * i < crossLen + 2*rad; ++i) { //
-            double relEdgeLen = (edgelen * (crossLen - (lenbtCircles) * i)) / (crossLen);
-            for (int j = 0; (lenbtCircles) * j < relEdgeLen + 2*rad; ++j) {
-                //adds search circles to one side of square
+        if(crossLen < 2000) {
+            ArrayList<LatLng> circleLoc = new ArrayList<>();
+            for (int i = 0; lenbtCircles * i < crossLen + 2 * rad; ++i) { //
+                double relEdgeLen = (edgelen * (crossLen - (lenbtCircles) * i)) / (crossLen);
                 circleLoc.add(new LatLng(
-                        marker_1.latitude + (lenbtCircles * i) * Math.sin(-Math.toRadians(45.0) + alpha)/correctLatitude,
-                        marker_1.longitude + (lenbtCircles * j) * Math.cos(-Math.toRadians(45.0) + alpha)/correctLongitude));
+                        marker_1.latitude + (lenbtCircles * i) * Math.sin(alpha) / correctLatitude,
+                        marker_1.longitude + (lenbtCircles * i) * Math.cos(alpha) / correctLongitude));
+                for (int j = 0; (lenbtCircles) * j < relEdgeLen + 2 * rad; ++j) {
+                    //adds search circles to one side of square
+                    circleLoc.add(new LatLng(
+                            marker_1.latitude + (lenbtCircles * i) * Math.sin(-Math.toRadians(45.0) + alpha) / correctLatitude,
+                            marker_1.longitude + (lenbtCircles * j) * Math.cos(-Math.toRadians(45.0) + alpha) / correctLongitude));
 
-                //adds to other side
-                circleLoc.add(new LatLng(
-                        marker_1.latitude + (lenbtCircles * i) * Math.sin(Math.toRadians(45.0) + alpha)/correctLatitude,
-                        marker_1.longitude + (lenbtCircles * j) * Math.cos(Math.toRadians(45.0) + alpha)/correctLongitude));
+                    //adds to other side
+                    circleLoc.add(new LatLng(
+                            marker_1.latitude + (lenbtCircles * i) * Math.sin(Math.toRadians(45.0) + alpha) / correctLatitude,
+                            marker_1.longitude + (lenbtCircles * j) * Math.cos(Math.toRadians(45.0) + alpha) / correctLongitude));
 
-            }
-        }
-
-        ArrayList<String> URLs = turnToURL(circleLoc);
-
-        ScrapJSON background = new ScrapJSON() {
-            @Override
-            public void onResponseReceived(Object result) {
-                if(result instanceof ArrayList){
-                    HeatmapTileProvider provide = new HeatmapTileProvider.Builder().weightedData((ArrayList)result).build();
-                    TileOverlay overlay = map.addTileOverlay(new TileOverlayOptions().tileProvider(provide));
                 }
             }
-        };
-        background.execute(URLs);
+            double dist = (1.0 / Math.cos(Math.toRadians(22.5))) * (crossLen / 2.0);
+            final LatLng checkRight = new LatLng((marker_1.latitude + dist * Math.sin(alpha - Math.toRadians(22.5)) / correctLatitude),
+                    marker_1.longitude + dist * Math.cos(alpha - Math.toRadians(22.5)) / correctLongitude);
+            final LatLng checkLeft = new LatLng((marker_1.latitude + dist * Math.sin(alpha + Math.toRadians(22.5)) / correctLatitude),
+                    marker_1.longitude + dist * Math.cos(alpha + Math.toRadians(22.5)) / correctLongitude);
+            final LatLng checkMid = new LatLng(marker_1.latitude + (crossLen / 2.0) * Math.sin(alpha) / correctLatitude,
+                    marker_1.longitude + (crossLen / 2.0) * Math.cos(alpha / correctLongitude));
+
+            final LatLng circleRad = new LatLng((crossLen / 6.0) / correctLatitude, (crossLen / 6.0) / correctLongitude);
+
+            ArrayList<String> URLs = turnToURL(circleLoc);
+
+            ScrapJSON background = new ScrapJSON() {
+                @Override
+                public void onResponseReceived(Object result) {
+                    if (result instanceof ArrayList) {
+                        ArrayList<WeightedLatLng> points = (ArrayList<WeightedLatLng>) result;
+                        HeatmapTileProvider provide = new HeatmapTileProvider.Builder().weightedData(points).build();
+                        TileOverlay overlay = map.addTileOverlay(new TileOverlayOptions().tileProvider(provide));
+                        ScrapRoadJSON road_background = new ScrapRoadJSON() {
+                            @Override
+                            public void onResponseReceived(Object result) {
+                                if (result != null && (result instanceof ArrayList)) {
+                                    ArrayList<LatLng> roadList = (ArrayList<LatLng>) result;
+                                    ArrayList<Double> roadPopul = new ArrayList<>();
+                                    for (int i = 0; i < roadList.size(); ++i) {
+                                        double popul = 0.0;
+                                        for (int j = 0; j < points.size(); ++j) {
+                                            if (points.get(j).getPoint().x > roadList.get(i).latitude - circleRad.latitude
+                                                    && points.get(j).getPoint().x < roadList.get(i).latitude + circleRad.latitude
+                                                    && points.get(j).getPoint().y > roadList.get(i).longitude - circleRad.longitude
+                                                    && points.get(j).getPoint().y < roadList.get(i).longitude + circleRad.longitude) {
+                                                popul += points.get(j).getIntensity();
+                                            }
+                                        }
+                                        roadPopul.add(popul);
+                                    }
+                                    int ind = -1;
+                                    if (option == 0) {
+                                        double max = -1.0;
+                                        for (int i = 0; i < roadPopul.size(); ++i) {
+                                            if (roadPopul.get(i) > max) {
+                                                max = roadPopul.get(i);
+                                                ind = i;
+                                            }
+                                        }
+                                    } else if (option == 1) {
+                                        double min = roadPopul.get(0);
+                                        ind = 0;
+                                        for (int i = 0; i < roadPopul.size(); ++i) {
+                                            if (roadPopul.get(i) < min) {
+                                                min = roadPopul.get(i);
+                                                ind = i;
+                                            }
+                                        }
+                                    }
+                                    ScrapDirectionJSON direction = new ScrapDirectionJSON() {
+                                        @Override
+                                        public void onResponseReceived(Object result) {
+                                            map.addPolyline(new PolylineOptions().addAll(((ArrayList<LatLng>) result)).visible(true).color(Color.BLACK));
+                                        }
+                                    };
+                                    direction.execute(turnToDirectionURL(marker_1, marker_2, roadList.get(ind)));
+                                }
+                            }
+                        };
+                        ArrayList<LatLng> checkPoints = new ArrayList<>();
+                        checkPoints.add(checkLeft);
+                        checkPoints.add(checkRight);
+                        checkPoints.add(checkMid);
+                        road_background.execute(turnToRoadURL(checkPoints));
+                    }
+                }
+            };
+            background.execute(URLs);
+        }
     }
 
     public double calcSlope() {
@@ -100,6 +170,29 @@ public class HeatMapCalc{
         }
 
         return URLs;
+    }
+
+    public static String turnToRoadURL(ArrayList<LatLng> locs){
+        StringBuilder url = new StringBuilder("https://roads.googleapis.com/v1/nearestRoads?points=");
+        for(int i = 0; i < locs.size(); ++i) {
+            if(i != 0)
+                url.append("|");
+            url.append(locs.get(i).latitude).append(",").append(locs.get(i).longitude);
+        }
+        url.append("&key=").append(App.getContext().getResources().getString(R.string.google_maps_key));
+        return url.toString();
+    }
+
+    public static String turnToDirectionURL(LatLng orig, LatLng dest, LatLng wpoint){
+
+        StringBuilder url = new StringBuilder("https://maps.googleapis.com/maps/api/directions/json?");
+        url.append("origin=").append(orig.latitude).append(",").append(orig.longitude).append("&");
+        url.append("destination=").append(dest.latitude).append(",").append(orig.longitude).append("&");
+        url.append("waypoints=").append(wpoint.latitude).append("%2C").append(wpoint.longitude).append("&");
+        url.append("mode=").append("walking").append("&");
+        url.append("key=").append(App.getContext().getResources().getString(R.string.google_maps_key));
+
+        return url.toString();
     }
 
     public double calcCorrectLongitude(double lat){
--- app/src/main/java/com/example/crowdedhackathon/MapsActivity.java
+++ app/src/main/java/com/example/crowdedhackathon/MapsActivity.java
@@ -85,7 +85,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onClick(View v) {
                 if(_markers.size() == 2){
                     try {
-                        new HeatMapCalc(_map, _markers, 300, 30);
+                        new HeatMapCalc(_map, _markers, 200, 30, 0);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        Button addLessHMap = findViewById(R.id.add_lessheat_map);
+        addLessHMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(_markers.size() == 2){
+                    try {
+                        new HeatMapCalc(_map, _markers, 200, 30, 1);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -95,15 +109,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         _map = googleMap;
